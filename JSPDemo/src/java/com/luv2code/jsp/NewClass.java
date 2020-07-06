9020079436b422be14ed899698c2603dc9dcb9cd
Nivesh Chandola <cnivesh@hotmail.com>
2020-07-06 20:58:29 +0530

    modified to repo

--- JSPDemo/src/java/com/luv2code/jsp/NewClass.java
+++ JSPDemo/src/java/com/luv2code/jsp/NewClass.java
@@ -1,7 +1,7 @@
 /*
- *  Document   : NewClass
- *  Created on : 25/08/2019, 8:58:01 PM
- *  Author     : Nivesh-GC
+ *  Document  : NewClass
+ *  Created on: 25/08/2019, 8:58:01 PM
+ *  Author    : Nivesh-GC
  */
 
 package com.luv2code.jsp;
