2c5b5f78cd56a28c5e9b509c505c459e275ea113
Nivesh Chandola <cnivesh@hotmail.com>
2020-07-06 09:42:52 +0530

    Update NewClass.java

--- JSPDemo/src/java/com/luv2code/jsp/NewClass.java
+++ JSPDemo/src/java/com/luv2code/jsp/NewClass.java
@@ -1,15 +1,8 @@
 /*
- *  @author		: Nivesh Chandola
- *  Created on	: 25/08/2019, 8:58:01 PM
- *  Program		: 	
- *  Last Edited	: 
+ *  Document   : NewClass
+ *  Created on : 25/08/2019, 8:58:01 PM
+ *  Author     : Nivesh-GC
  */
-
-/*
-	Document   : NewClass
-    Created on : 25/08/2019, 8:58:01 PM
-    Author     : Nivesh-GC
-*/
 
 package com.luv2code.jsp;
 
